cac1769718d3de647038593b9846d0ba8d720498
Victor Olamide <akinnusivictor098@gmail.com>
2023-03-24 15:25:25 +0100

    Algorithms

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -1,34 +1,74 @@
+import java.security.SecureRandom;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class BinarySearch {
-    public static void main(String[] args) {
 
+    public static int binarySearch(int[] data, int key) {
+        int low = 0;
+        int high = data.length - 1;
+        int mid = (low + high + 1) / 2;
+        int location = -1;
 
+        do{
+            System.out.print(remainingElements(data, low,high));
+            for(int i = 0; i < mid; i++){
+                System.out.print(" ");
+            }
+            System.out.println(" * ");
 
-
-        int [] numbers = {1,2,3,4,5,6,7,8};
-
-
+            if(key == data[mid]){
+                location =  mid;
+            }
+            else if(key < data[mid]){
+                high = mid -1;
+            }else {
+                low= mid + 1;
+            }
+            mid = (low + high + 1) / 2;
+        }while((low <= high) && (location == -1));
+        return location;
     }
 
+    private static String remainingElements(int[] data, int low, int high) {
 
+        StringBuilder temporary = new StringBuilder();
 
-    public static Object binarySearch(int[] numbers, int key){
-         int low = 0;
-         int high = numbers.length - 1;
-         int mid;
+        for(int i = 0; i < low; i++){
+            temporary.append(" ");
+        }
+        for( int i =low;i <= high; i++){
+            temporary.append(data[i] + " ");
+        }
+        return  String.format("%s%n",temporary);
+    }
 
-         while(low <= high){
-             mid = low + high / 2;
-             int value = numbers[mid];
-             if(value == key){
-                 return mid;
-             }else if (value > key) {
-                 high = mid - 1;
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        SecureRandom generator = new SecureRandom();
 
-             }else {
-                 low = mid + 1;
-             }
+        int []data = generator.ints(15,10,
+                91).sorted().toArray();
 
-         }
-        return null;
+        System.out.printf("%s%n%n", Arrays.toString(data));
+
+        System.out.print("Please enter an integer value(-1 to quit): ");
+        int searchInt = input.nextInt();
+
+        while (searchInt != -1) {
+
+            int location = binarySearch(data, searchInt);
+
+            if(location == -1){
+                System.out.printf("%d was not found%n%n", searchInt);
+            }else{
+                System.out.printf("%d was found in position %d%n%n", searchInt, location);
+
+            }
+
+            System.out.print("Please enter an integer value(-1 to quit: ");
+            searchInt = input.nextInt();
+        }
+
     }
 }
